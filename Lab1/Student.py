c925b6bcfeba612feb8b73c4ea95f4fdf2dd609a
Crumbtray <clint.isaac.wong@gmail.com>
2014-01-15 16:28:13 -0700

    Finished Lab 1

--- Lab1/Student.py
+++ Lab1/Student.py
@@ -1,14 +1,23 @@
-class Student
+class Student:
     courseMarks={}
     name=""
+    family=""
 
-    def __init__(self, courseName):
+    def __init__(self, name, family):
         self.name = name
+        self.family = family
 
     def addCourseMark(self, course, mark):
         self.courseMarks[course] = mark
 
     def average(self):
-        
+        marks = self.courseMarks.values()
+        average = sum(marks) / len(marks)
+        return average
 
-
+e = Student("jimmy", "neutron")
+print e.name
+print e.family
+e.addCourseMark("EE 240", 30)
+e.addCourseMark("CMPUT 420", 2) 
+print e.average()
